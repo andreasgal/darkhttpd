cc0d7342b16a6c69bba74a4644d27c4855a140e8
Andreas Gal <gal@mozilla.com>
2012-02-19 20:50:08 +0100

    first commit

---
+++ Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=\
+	darkhttpd.c
+
+LOCAL_CFLAGS := -O2 -g
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_MODULE := darkhttpd
+
+include $(BUILD_EXECUTABLE)
